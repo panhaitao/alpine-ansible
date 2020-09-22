d4fc63f5a132952e614cf13febc4b25a8a06b2f7
潘海涛 <xz@onwalk.net>
2020-09-22 20:31:18 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,3 +2,9 @@
 mkdir -p /run/nginx/
 touch /run/nginx/nginx.pid
 nginx -g "daemon off;" 
+
+
+#ssh-keygen -t rsa -b 2048 -f /etc/ssh/ssh_host_rsa_key
+#ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
+#ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
+#ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ed25519_key
