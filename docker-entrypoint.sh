551a44b0b7af9b971e1b5e16456438723c1be93b
潘海涛 <xz@onwalk.net>
2020-09-22 20:35:41 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
+mkdir -pv /data/ 
+cd /data && git clone https://github.com/panhaitao/ansible-playbook-store.git playbook
+
 mkdir -p /run/nginx/
 touch /run/nginx/nginx.pid
 nginx -g "daemon off;" 
-
+chown root:nginx /var/www/localhost/* 
 
 #ssh-keygen -t rsa -b 2048 -f /etc/ssh/ssh_host_rsa_key
 #ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
