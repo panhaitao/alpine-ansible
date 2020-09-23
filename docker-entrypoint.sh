020ed332475b736e586daaa0898b38bad479afb3
潘海涛 <xz@onwalk.net>
2020-09-23 11:20:44 +0800

    Add sshd service

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,8 +8,8 @@ touch /run/nginx/nginx.pid
 nginx -g "daemon off;" 
 chown root:nginx /var/www/localhost/* 
 
-#ssh-keygen -t rsa -b 2048 -f /etc/ssh/ssh_host_rsa_key
-#ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
-#ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key
-#ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ed25519_key
-#/usr/sbin/sshd -f /etc/ssh/sshd_config
+ssh-keygen -t rsa -b 2048 -f /etc/ssh/ssh_host_rsa_key -P "" -q
+ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -P "" -q
+ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -P "" -q
+ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ed25519_key -P "" -q
+/usr/sbin/sshd -f /etc/ssh/sshd_config
